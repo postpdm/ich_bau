862364592ffd0352c173bdd1e571660e1be1880c
yn_coder <yn.coder@gmail.com>
2017-10-01 21:55:46 +0300

    #26 create svn repo with svnadmin

--- project/repo_wrapper.py
+++ project/repo_wrapper.py
@@ -1,9 +1,14 @@
 # wrapper for SVN
 
 import svn.remote
+import svn.admin
 
-REPO_BASE_URL = 'file:///d:/test/'
-REPO_LOCAL_ROOT = ''
+import uuid
+
+REPO_BASE_URL = 'file:///d:/test/repos/'
+REPO_LOCAL_ROOT = "d:\\test\\repos\\"
+
+SVN_ADMIN_FULL_PATH = 'd:\\test\\svn\\VisualSVN Server\\bin\\svnadmin.exe'
 
 def Get_Info_For_Repo_Name( arg_repo_name ):
     r = svn.remote.RemoteClient( REPO_BASE_URL + arg_repo_name )
@@ -13,11 +18,17 @@ def Get_Info_For_Repo_Name( arg_repo_name ):
 
 VCS_CREATE_REPO_SUCCESS = 0
 VCS_CREATE_REPO_FAIL_NOT_CONFIGURED = 1
+VCS_CREATE_REPO_FAIL_CALL = 2
 
 # return (code, str)
 def Create_New_Repo( ):
-    #return ( VCS_CREATE_REPO_FAIL_NOT_CONFIGURED, 'file:///d:/test/repo' )
-    return ( VCS_CREATE_REPO_SUCCESS, 'repo' )
+    try:
+        repo_guid_name = uuid.uuid4().hex
+        a = svn.admin.Admin( svnadmin_filepath = SVN_ADMIN_FULL_PATH )
+        a.create( REPO_LOCAL_ROOT + repo_guid_name, svnadmin_filepath = SVN_ADMIN_FULL_PATH )
+        return ( VCS_CREATE_REPO_SUCCESS, repo_guid_name )
+    except:
+        return ( VCS_CREATE_REPO_FAIL_CALL, '' )
     
 def Get_List_For_Repo_Name( arg_repo_name ):
     r = svn.remote.RemoteClient( REPO_BASE_URL + arg_repo_name )
