aa723cf041ea24e9f0a802251d6bf7533211a965
yn_coder <yn.coder@gmail.com>
2017-10-06 10:48:46 +0300

    #31 templatetag without context

--- project/templatetags/template_arithmetic_tag.py
+++ project/templatetags/template_arithmetic_tag.py
@@ -4,8 +4,8 @@ register = template.Library()
 
 from django.utils.html import format_html
 
-@register.simple_tag(name='percent', takes_context=True)
-def percent(context, arg1, arg2):
+@register.simple_tag(name='percent', takes_context=False)
+def percent(arg1, arg2):
     if ( arg1 == 0 ) or ( arg2 == 0 ):
         return 0
     else:    
