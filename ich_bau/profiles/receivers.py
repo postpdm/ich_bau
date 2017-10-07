952c5e3f9db5276f7df5ae3c7874fa544a1e8f95
yn_coder <yn.coder@gmail.com>
2017-10-07 10:21:45 +0300

    trim spaces

--- ich_bau/profiles/receivers.py
+++ ich_bau/profiles/receivers.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 
 from .models import Profile
 
-
 @receiver(post_save, sender=User)
 def handle_user_save(sender, created, instance, **kwargs):
     if created:
