b6a2eca045d1be1cb179d3eaff55b361ed1c18b5
yn_coder <yn.coder@gmail.com>
2017-10-07 11:19:08 +0300

    #26 Simple tests for `Repo_File_Paths`

--- project/test_svn_wrapper.py
+++ project/test_svn_wrapper.py
@@ -1,5 +1,5 @@
 from project.repo_wrapper import *
-  
+
 from unittest import TestCase
 
 class SVN_Wrapper_Test(TestCase):
@@ -10,3 +10,11 @@ class SVN_Wrapper_Test(TestCase):
     def test_Get_List(self):
         res = Get_List_For_Repo_Name( 'some wrong repo' )
         self.assertEqual( res[0], VCS_REPO_FAIL_CALL )
+
+    def test_Repo_Conf_Name_Gen(self):
+        s = ''
+        t = Repo_File_Paths( s )
+
+        self.assertEqual( t.svnserve_conf_full_name( ), s + '\\conf\\svnserve.conf' )
+        self.assertEqual( t.pass_full_name( ),          s + '\\conf\\passwd' )
+        self.assertEqual( t.auth_full_name( ),          s + '\\conf\\authz' )
