33ff8c48dcca166ed0abcf12e1352f8f1c77c9ac
yn_coder <yn.coder@gmail.com>
2017-10-07 11:45:42 +0300

    some tests for profiles

---
+++ ich_bau/profiles/test_profiles.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from .models import *
+
+from unittest import TestCase
+
+BOT_TEST_NAME = 'BOT TEST NAME'
+
+class Profile_Test(TestCase):
+    def setUp(self):
+        if not User.objects.filter( username = 'bot' ).exists():
+            bot = User.objects.create_user( username = 'bot', password = '123' )
+            bot_profile = bot.profile
+            bot_profile.profile_type = PROFILE_TYPE_BOT
+            bot_profile.name = BOT_TEST_NAME
+            bot_profile.save()
+
+    def test_Bot_profile_type(self):
+        bot_profile = Profile.objects.get(name=BOT_TEST_NAME)
+        self.assertEqual( bot_profile.profile_type, PROFILE_TYPE_BOT )
+
+    def test_Bot_repo_pw(self):
+        bot_profile = Profile.objects.get(name=BOT_TEST_NAME)
+        self.assertFalse( bot_profile.repo_pw is None )
