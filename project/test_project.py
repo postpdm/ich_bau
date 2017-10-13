e9ca7e080cf872393104b81ab503910c07549e1c
yn_coder <yn.coder@gmail.com>
2017-10-13 23:08:23 +0300

    test - for user membership

--- project/test_project.py
+++ project/test_project.py
@@ -8,6 +8,7 @@ import reversion
 #from reversion.models import Version
 
 TEST_USER_NAME_CREATOR = 'test project creator'
+TEST_USER_NAME_NOT_MEMBER = 'user is not a member'
 TEST_PROJECT_NAME = 'test project name'
 
 def get_project():
@@ -16,10 +17,16 @@ def get_project():
 def get_creator_user():
     return User.objects.get( username = TEST_USER_NAME_CREATOR )
 
+def get_user_not_member():
+    return User.objects.get( username = TEST_USER_NAME_NOT_MEMBER )
+
 class Project_Test(TestCase):
     def setUp(self):
         user_creator = User.objects.create_user( username = TEST_USER_NAME_CREATOR, password = '-' )
         user_creator.save()
+
+        user_not_member = User.objects.create_user( username = TEST_USER_NAME_NOT_MEMBER, password = '-' )
+        user_not_member.save()
 
         test_project = Project(fullname=TEST_PROJECT_NAME)
         test_project.set_change_user(user_creator)
@@ -34,6 +41,15 @@ class Project_Test(TestCase):
         user_creator = get_creator_user()
         self.assertEqual( test_project.is_member(user_creator), True )
 
+    def test_creator_is_member_False(self):
+        test_project = get_project()
+        user_not_member = get_user_not_member()
+        self.assertEqual( test_project.is_member(user_not_member), False )
+
+    def test_creator_is_member_None(self):
+        test_project = get_project()        
+        self.assertEqual( test_project.is_member(None), False )
+
     def test_creator_is_admin(self):
         test_project = get_project()
         user_creator = get_creator_user()
@@ -43,3 +59,12 @@ class Project_Test(TestCase):
         test_project = get_project()
         user_creator = get_creator_user()
         self.assertEqual( test_project.can_admin(user_creator), True )
+
+    def test_creator_acl_admin(self):
+        test_project = get_project()
+        user_creator = get_creator_user()
+        self.assertEqual( test_project.user_access_level(user_creator), PROJECT_ACCESS_ADMIN )
+
+    def test_none_user_acl_admin(self):
+        test_project = get_project()
+        self.assertEqual( test_project.user_access_level( None ), PROJECT_ACCESS_VIEW )
