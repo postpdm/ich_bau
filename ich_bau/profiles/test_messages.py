f9c0eb4c4c67aa40980cd0bea754328bd1e3703b
yn_coder <yn.coder@gmail.com>
2017-10-16 07:19:34 +0300

    #28 tests for notifications

--- ich_bau/profiles/test_messages.py
+++ ich_bau/profiles/test_messages.py
@@ -9,7 +9,7 @@ class Message_Test(TestCase):
         s = project_msg2json_str( MSG_NOTIFY_TYPE_PROJECT_CHANGED_ID, arg_project_name = 'some project' )
         self.assertEqual( s, '{"msg_type": 2, "project_name": "some project"}' )
         self.assertEqual( decode_json2msg(s), "Changes in the 'some project' project." )
-        
+
     def test_Encode_Project_MSG_Fail(self):
         s = project_msg2json_str( -1, arg_project_name = '*' )
         self.assertFalse( s )
