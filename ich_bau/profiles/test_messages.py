7f04ec2619b7a2b760fd606e9f2738b2dd498329
yn_coder <yn.coder@gmail.com>
2017-10-17 07:55:34 +0300

    #15 User want to join - not completed

--- ich_bau/profiles/test_messages.py
+++ ich_bau/profiles/test_messages.py
@@ -7,7 +7,7 @@ from .messages import *
 class Message_Test(TestCase):
     def test_Encode_Decode_Project_MSG(self):
         s = project_msg2json_str( MSG_NOTIFY_TYPE_PROJECT_CHANGED_ID, arg_project_name = 'some project' )
-        self.assertEqual( s, '{"msg_type": 2, "project_name": "some project"}' )
+        self.assertEqual( s, '{"msg_type": 20, "project_name": "some project"}' )
         self.assertEqual( decode_json2msg(s), "Changes in the 'some project' project." )
 
     def test_Encode_Project_MSG_Fail(self):
