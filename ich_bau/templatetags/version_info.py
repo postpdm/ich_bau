7e2446ae84433a49d7ee920b0e22e9b2b061b9b9
yn_coder <yn.coder@gmail.com>
2017-11-08 04:50:13 +0300

    #28 simple test for version info

--- ich_bau/templatetags/version_info.py
+++ ich_bau/templatetags/version_info.py
@@ -2,9 +2,6 @@
 
 register = template.Library()
 
-#from django.utils.html import format_html
-
 @register.simple_tag(name='site_version_info')
 def site_version_info():
-    return 'v0.0 at 20.10.2017'
-    #return #format_html('<i class="fa ' + PROFILE_TYPE_ICONS[ arg ][1] + ' fa-4x"></i>')
+    return 'v0.0001 at 07.11.2017'
