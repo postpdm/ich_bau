4d1dc36dba566d60d82f6f1832c121c205a7e3fe
yn_coder <yn.coder@gmail.com>
2018-05-04 01:11:11 +0300

    #58 first test for profile affiliation

--- ich_bau/profiles/test_profiles_rel.py
+++ ich_bau/profiles/test_profiles_rel.py
@@ -10,8 +10,14 @@ class Profile_Test(TestCase):
         self.main_org_profile = Profile( profile_type = PROFILE_TYPE_ORG )
         self.main_org_profile.name = TEST_MAIN_ORG_NAME
         self.main_org_profile.save()
+        
+        for i in ( 'a', 'b', 'c' ):
+            sub_dep_profile = Profile( profile_type = PROFILE_TYPE_DEPARTAMENT )
+            sub_dep_profile.name = i
+            sub_dep_profile.save()
+            r = Profile_Affiliation( main_profile=self.main_org_profile, sub_profile=sub_dep_profile )
+            r.save()
 
     def test_Org_profile_type(self):
         self.assertEqual( self.main_org_profile.profile_type, PROFILE_TYPE_ORG )
-
-        
+        self.assertEqual( Profile_Affiliation.objects.filter(main_profile=self.main_org_profile).count(), 3 )
