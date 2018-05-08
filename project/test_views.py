1a5ce448e4c8cad2f80dcaf97f10cdb7a4e6a924
yn_coder <yn.coder@gmail.com>
2018-05-08 23:48:13 +0300

    #28 integration test for project creation and searching

--- project/test_views.py
+++ project/test_views.py
@@ -4,13 +4,66 @@ from django.test import TestCase, Client
 
 from django.core.urlresolvers import reverse
 
+from .models import Project
+
+TEST_USER_NAME  = 'test_user'
+TEST_USER_EMAIL = 'test_user@nothere.com'
+TEST_USER_PW    = 'test_user_pw'
+
+TEST_PROJECT_FULLNAME = 'TEST PROJECT #1 FULL NAME'
+
 class Project_View_Test_Client(TestCase):
     def test_Project_All_Public(self):
         c = Client()
         response = c.get( reverse('project:all_public') )
         self.assertEqual( response.status_code, 200 )
 
+    def test_Search_Project_Page_is_200(self):
+        c = Client()
+        response = c.get( reverse('project:search_public') )
+        self.assertContains(response, '0 found.', status_code=200 )
+
     def test_Project_Index(self):
         c = Client()
         response = c.get( reverse('project:index') )
         self.assertContains(response, 'Please, log in to see your projects.', status_code=200 )
+
+    def test_Long_Long(self):
+        # no projects here
+        self.assertEqual( Project.objects.count(), 0 )
+
+        # create user
+        if not User.objects.filter( username = TEST_USER_NAME ).exists():
+            test_user = User.objects.create_user( username = TEST_USER_NAME, password = TEST_USER_PW )
+
+        c = Client()
+        # check for project search page is available for anon
+        response = c.get( reverse('project:search_public'), { 'fullname' : TEST_PROJECT_FULLNAME } )
+        self.assertContains(response, '0 found.', status_code=200 )
+
+        # log in
+        res = c.login( username = TEST_USER_NAME, password = TEST_USER_PW )
+        self.assertTrue( res )
+
+        # create new project with post
+        response = c.post( reverse('project:project_add'), { 'fullname' : TEST_PROJECT_FULLNAME, 'description' : '', } )
+        # we are redirected to new project page
+        self.assertEqual( response.status_code, 302 )
+
+        # check project is created
+        self.assertEqual( Project.objects.count(), 1 )
+        test_project_1 = Project.objects.get(id=1)
+        self.assertEqual( response.url, test_project_1.get_absolute_url() )
+        self.assertEqual( test_project_1.fullname, TEST_PROJECT_FULLNAME )
+        self.assertTrue( test_project_1.is_member( test_user ) )
+
+        # check - new project is available in search page
+        response = c.get( reverse('project:search_public'), { 'fullname' : TEST_PROJECT_FULLNAME } )
+        self.assertContains(response, '1 found.', status_code=200 )
+
+        response = c.get( reverse('project:search_public'), { 'fullname' : '-no-', 'description' : '-no-' } )
+        self.assertContains(response, '0 found.', status_code=200 )
+
+        # check - project page is available
+        response = c.get( reverse('project:project_view', args = (1,) ) )
+        self.assertContains(response, TEST_PROJECT_FULLNAME, status_code=200 )
