d20b458f74cfca27ad01203d5f70d5f3e1f5225e
yn_coder <yn.coder@gmail.com>
2018-05-19 00:59:27 +0300

    #32 simple_crypt was replaced with django-cryptography (to  9b40564)

--- commons/test_simple_crypt.py
+++
@@ -1,9 +0,0 @@
-from .simple_crypt import *
-
-from unittest import TestCase
-
-class Simple_Crypt_Test(TestCase):
-    def test_Simple_Crypt(self):
-        s = '12345'
-        p = 'udp3-wxwex4'
-        self.assertEqual( DeCrypt_Str( EnCrypt_Str( s, p ) , p ), s )
