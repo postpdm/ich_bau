9d0af5c0c8201f70ccbfeb84134597910231d9e5
yn_coder <yn.coder@gmail.com>
2018-11-11 00:14:47 +0300

    #80 extend project list in admin panel

--- project/admin.py
+++ project/admin.py
@@ -4,4 +4,12 @@ from .models import Project
 
 admin.site.register(
     Project,
-)
+    list_display=[
+        "fullname",
+        "active_flag",
+        "private_flag",
+        "created_at",
+        "created_user",
+        "modified_at",
+        "modified_user",
+    ],)
