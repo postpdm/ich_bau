b80e08b3b2619054b9acf01b001a791729cf87e2
yn_coder <yn.coder@gmail.com>
2018-11-15 07:04:09 +0300

    #68 gitignore django-summernote media folder. Fis the error in task create test and fix the summernore widget (forgot for required arg)

--- commons/editors.py
+++ commons/editors.py
@@ -5,7 +5,7 @@ from bootstrap3_datetime.widgets import DateTimePicker
 from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
 def TextEditor_Field(arg_required=False):
-    return forms.CharField(widget=SummernoteWidget())
+    return forms.CharField(required=arg_required, widget=SummernoteWidget())
 
 def DateTime_Field(arg_required=False):
     return forms.DateField( required = False, widget=DateTimePicker(options={"format": "YYYY-MM-DD", "pickTime": False}))
