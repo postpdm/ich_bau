1280a9d00c502a2b59c18cc224006025f9f22cac
yn_coder <yn.coder@gmail.com>
2018-11-16 21:25:07 +0300

    #89

--- project/test_views.py
+++ project/test_views.py
@@ -17,6 +17,7 @@ TEST_PROJECT_DESCRIPTION_2 = 'Second description'
 TEST_TASK_FULLNAME = 'TEST TASK #1 FULL NAME'
 
 TEST_MILESTONE_FULLNAME = 'TEST MILESTONE #1 FULL NAME'
+TEST_MILESTONE_FULLNAME_2 = 'TEST MILESTONE #1 NEW FULL NAME'
 
 class Project_View_Test_Client(TestCase):
     def test_Project_All_Public(self):
@@ -179,3 +180,11 @@ class Project_View_Test_Client(TestCase):
         response = c.get( reverse_lazy('project:milestone_history', args = (test_ms_1.id,) ) )
         self.assertContains(response, TEST_MILESTONE_FULLNAME, status_code=200 )
 
+        # edit milestone
+
+        response = c.post( reverse_lazy('project:milestone_edit', args = (test_ms_1.id,) ), { 'fullname' : TEST_MILESTONE_FULLNAME_2, } )
+        # we are redirected to edited milestone page
+        self.assertEqual( response.status_code, 302 )
+        self.assertEqual( Milestone.objects.count(), 1 )
+        test_ms_1.refresh_from_db()
+        self.assertEqual( test_ms_1.fullname, TEST_MILESTONE_FULLNAME_2 )
