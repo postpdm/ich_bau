142f1c90cffe830d1c397e87aa9c1d5140573d76
yn_coder <yn.coder@gmail.com>
2018-11-18 00:14:28 +0300

    #89 test `project:task_checklist` with 0

--- project/test_views.py
+++ project/test_views.py
@@ -188,3 +188,7 @@ class Project_View_Test_Client(TestCase):
         self.assertEqual( Milestone.objects.count(), 1 )
         test_ms_1.refresh_from_db()
         self.assertEqual( test_ms_1.fullname, TEST_MILESTONE_FULLNAME_2 )
+
+        # task_checklist
+        response = c.get( reverse_lazy('project:task_checklist', args = (0,) ) )
+        self.assertEqual( response.status_code, 404 )
