977328fe4b3093aa8b1afef4e6dc5ee38252c327
yn_coder <yn.coder@gmail.com>
2018-11-25 21:33:24 +0300

    #99 Get_Log_For_Repo_Name for VCS_REPO_FAIL_NOT_CONFIGURED

--- project/test_svn_wrapper.py
+++ project/test_svn_wrapper.py
@@ -118,6 +118,7 @@ class SVN_Wrapper_Overwrite_Settings(SimpleTestCase):
             self.assertTrue( settings.REPO_SVN.get('REPO_TYPE') == None )
             self.assertFalse( VCS_Configured() )
             self.assertTrue( Get_Info_For_Repo_Name( 'meaningless name' )[0] == VCS_REPO_FAIL_NOT_CONFIGURED )
+            self.assertTrue( Get_Log_For_Repo_Name( 'meaningless name' )[0] == VCS_REPO_FAIL_NOT_CONFIGURED )
 
     def test_Overwrite_Settings_File_Protocol(self):
         path =  tempfile.gettempdir()
