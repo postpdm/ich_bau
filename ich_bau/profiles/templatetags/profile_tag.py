45c2a260c379d5eb94c58e364c40ca709f90be8e
yn_coder <yn.coder@gmail.com>
2018-12-03 22:53:43 +0300

    #103 profile list is organized in groups

--- ich_bau/profiles/templatetags/profile_tag.py
+++ ich_bau/profiles/templatetags/profile_tag.py
@@ -7,7 +7,7 @@ from django.utils.html import format_html
 from ich_bau.profiles.models import *
 
 PROFILE_TYPE_ICONS = (
-  ( PROFILE_TYPE_BOT, 'fa-meh-o' ), 
+  ( PROFILE_TYPE_BOT, 'fa-meh-o' ),
   ( PROFILE_TYPE_USER, 'fa-user' ),
   ( PROFILE_TYPE_PEOPLE, 'fa-bars' ),
   ( PROFILE_TYPE_DEPARTAMENT, 'fa-users' ),
@@ -18,3 +18,7 @@ PROFILE_TYPE_ICONS = (
 @register.simple_tag(name='fa_profile_icon', takes_context=True)
 def fa_profile_icon(context, arg):
     return format_html('<i class="fa ' + PROFILE_TYPE_ICONS[ arg ][1] + ' fa-4x"></i>')
+
+@register.simple_tag(name='profile_type_name', takes_context=True)
+def profile_type_name(context, arg):
+    return PROFILE_TYPE_CHOICES[arg][1]
