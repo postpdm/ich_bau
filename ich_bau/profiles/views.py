f7142711d620b430b2f54791a813b49835777357
yn_coder <yn.coder@gmail.com>
2019-09-18 23:36:45 +0300

    #110 group notifications by sender

--- ich_bau/profiles/views.py
+++ ich_bau/profiles/views.py
@@ -103,7 +103,7 @@ def notifications_view_prepare(request, arg_new):
     if u is None:
         raise Http404
 
-    notifications = GetUserNoticationsQ( u, arg_new )
+    notifications = GetUserNoticationsQ( u, arg_new ).order_by('sender_user')
 
     context_dict = { 'notifications' : notifications, 'filter_new' : arg_new }
     return render( request, 'profiles/notifications.html', context_dict )
