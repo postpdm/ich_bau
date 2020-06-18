cba300d784607354b9cae8b14925028b29afbc0c
yn-coder <yn.coder@gmail.com>
2020-06-18 07:41:49 +0300

    #194 auto test for mail message - task assigned

--- ich_bau/profiles/test_messages.py
+++ ich_bau/profiles/test_messages.py
@@ -74,3 +74,30 @@ class Message_Test(TestCase):
         self.assertEqual( e_letter.subject, 'You are asked to accept the membership of \'test\' project team!' )
 
         self.assertIn( 'You are asked to accept the membership of &#39;test&#39; project team!', e_letter.body )
+
+    def test_Send_Notification_Check_Email_TASK_ASSIGN(self):
+
+        mail.outbox = []
+        self.assertEqual(len(mail.outbox), 0)
+
+        settings.EMAIL_HOST_USER = SERVER_EMAIL
+
+        test_user = User.objects.create_user( username = TEST_USER_NAME, password = TEST_USER_PW, email = TEST_USER_EMAIL )
+        self.assertEqual( GetUserNoticationsQ( test_user, True).count(), 0 )
+
+        user_type = ContentType.objects.get(app_label='auth', model='user')
+
+        msg_str = project_msg2json_str( MSG_NOTIFY_TYPE_PROJECT_TASK_ASSIGNED_ID, arg_project_name = 'test', arg_task_name = 'test_task' )
+        self.assertEqual( msg_str, '{"msg_type": 45, "project_name": "test", "task_name": "test_task"}' )
+
+        Send_Notification( test_user, test_user, user_type, 1, MSG_NOTIFY_TYPE_PROJECT_TASK_ASSIGNED_ID, msg_str, 'Arg_Url' )
+        self.assertEqual( GetUserNoticationsQ( test_user, True).count(), 1 )
+        self.assertEqual(len(mail.outbox), 1)
+
+        e_letter = mail.outbox[0]
+
+        self.assertEqual( e_letter.from_email, SERVER_EMAIL )
+        self.assertIn( TEST_USER_EMAIL, e_letter.to )
+        self.assertEqual( e_letter.subject, 'You are assigned at a \'test_task\' task of \'test\' project.' )
+
+        self.assertIn( 'You are assigned at a &#39;test_task&#39; task of &#39;test&#39; project', e_letter.body )
