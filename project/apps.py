8696ef7a3545b6b30c22ed0af695970a5ce24a76
user <user@Y.local>
2020-09-02 15:06:59 +0300

    #223 - templated

--- project/apps.py
+++ project/apps.py
@@ -1,18 +1,14 @@
 from commons.apps import BaseAppConfig
+from django.template.loader import render_to_string
 
 class AppConfig(BaseAppConfig):
     name = 'project'
 
     def get_site_index_html_block( self, request ):
-
         from project.models import GetMemberedProjectList
         if request.user.is_authenticated:
-            html = ''
+
             projects = GetMemberedProjectList(request.user)
-            for p in projects:
-                html = html + '<li> <a href="' + p.get_absolute_url() + '">' + p.fullname + '</a></li>'
-            if html:
-                html = '<p>Your projects</p><ul>' + html + '</ul>'
-            return html
+            return render_to_string( 'project/site_index_project_list.html', { 'projects' : projects } )
         else:
             return ''
