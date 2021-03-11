9b52b478f03302f96ca6696aedb35c5fd0328f68
yn-coder <yn.coder@gmail.com>
2021-03-11 14:05:40 +0300

    #180 first step for tests for Sub projects views

--- project/test_views.py
+++ project/test_views.py
@@ -343,3 +343,33 @@ class Project_View_Test_Client(TestCase):
 
         self.assertContains(response, test_task_1.fullname, status_code=200 )
         self.assertContains(response, test_task_2.fullname, status_code=200 )
+
+    def test_sub_projects(self):
+        # no projects here
+        self.assertEqual( Project.objects.count(), 0 )
+
+        # create user
+        if not User.objects.filter( username = TEST_USER_NAME ).exists():
+            test_user = User.objects.create_user( username = TEST_USER_NAME, password = TEST_USER_PW )
+
+        c = Client()
+        # log in
+        res = c.login( username = TEST_USER_NAME, password = TEST_USER_PW )
+        self.assertTrue( res )
+
+        add_project_permission = Permission.objects.get(codename='add_project')
+        test_user.user_permissions.add( add_project_permission )
+
+        response = c.post( reverse_lazy('project:project_add'), { 'fullname' : TEST_PROJECT_FULLNAME, 'private_type' : PROJECT_VISIBLE_VISIBLE, 'description' : TEST_PROJECT_DESCRIPTION_1, } )
+        # we are redirected to new project page
+        self.assertEqual( response.status_code, 302 )
+
+        # check project is created
+        self.assertEqual( Project.objects.count(), 1 )
+        test_project_1 = Project.objects.get(id=1)
+        self.assertEqual( response.url, test_project_1.get_absolute_url() )
+        self.assertEqual( test_project_1.fullname, TEST_PROJECT_FULLNAME )
+        self.assertEqual( test_project_1.description, TEST_PROJECT_DESCRIPTION_1 )
+        self.assertTrue( test_project_1.is_member( test_user ) )
+        self.assertFalse( test_project_1.use_sub_projects )
+
