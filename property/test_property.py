d3fe422204066087fed495bcf0c7d0da7c1f7ef1
yn-coder <yn.coder@gmail.com>
2021-03-17 13:36:35 +0300

    #16 #260 test for EnumerableProperty

--- property/test_property.py
+++ property/test_property.py
@@ -10,15 +10,26 @@ import reversion
 TEST_USER_NAME_CREATOR = 'test project creator'
 TEST_QUANTITY_NAME = 'Mass'
 
-TEST_ENUMERABLEPROPERTY_NAME = 'Variable'
+TEST_ENUMERABLEPROPERTY_NAME = 'Type'
+TEST_ENUMERABLEVARIANTS_NAME = 'Aero'
 
 class Property_Test(TestCase):
 
     def test_EnumerableProperty(self):
-        q = EnumerableProperty( fullname = TEST_ENUMERABLEPROPERTY_NAME )
-        q.save()
+        ep = EnumerableProperty( fullname = TEST_ENUMERABLEPROPERTY_NAME )
+        ep.save()
         self.assertEqual( EnumerableProperty.objects.count(), 1 )
-        self.assertEqual( str( q ), TEST_ENUMERABLEPROPERTY_NAME )
+        self.assertEqual( str( ep ), TEST_ENUMERABLEPROPERTY_NAME )
+
+    def test_EnumerableVariants(self):
+        ep = EnumerableProperty( fullname = TEST_ENUMERABLEPROPERTY_NAME )
+        ep.save()
+
+        ev = EnumerableVariants( fullname = TEST_ENUMERABLEVARIANTS_NAME, enumerable_property = ep )
+        ev.save()
+        self.assertEqual( EnumerableVariants.objects.count(), 1 )
+        self.assertEqual( str( ev ), TEST_ENUMERABLEVARIANTS_NAME )
+        self.assertEqual( str( ev.enumerable_property ), TEST_ENUMERABLEPROPERTY_NAME )
 
     def test_Quantity(self):
         q = Quantity( fullname = TEST_QUANTITY_NAME )
