bd7e6e85ff9f185ddd2ced14bf5dc88ea835dfd0
yn-coder <yn.coder@gmail.com>
2021-03-17 08:06:54 +0300

    #16 #260 first simple test for quantity

---
+++ property/test_property.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AnonymousUser
+from .models import *
+
+from django.test import TestCase
+
+from django.db import transaction
+
+import reversion
+
+TEST_USER_NAME_CREATOR = 'test project creator'
+TEST_QUANTITY_NAME = 'Mass'
+
+class Property_Test(TestCase):
+
+    def test_Quantity(self):
+        q = Quantity( fullname = TEST_QUANTITY_NAME )
+        q.save()
+        self.assertEqual( Quantity.objects.count(), 1 )
